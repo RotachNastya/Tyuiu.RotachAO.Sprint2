b9d50bdf9821e8e5fd94476f103770c2f956b626
RotachNastya <education.for.nastya@gmail.com>
2024-10-19 14:10:42 +0500

    Task1

---
+++ Tyuiu.RotachAO.Sprint2.Task1.V13.Lib/DataService.cs
@@ -0,0 +1,19 @@
+using tyuiu.cources.programming.interfaces.Sprint2; 
+ 
+namespace Tyuiu.RotachAO.Sprint2.Task1.V13.Lib;
+
+public class DataService : ISprint2Task1V13
+{
+    public bool[] GetLogicOperations(int a, int b, int c, int d)
+    {
+        bool[] res = new bool[6];
+        res[0] = (a > b) | (c < b);
+        res[1] = (a + 1000 > b) & (c < d + 100);
+        res[2] = (a > b) || (c < b);
+        res[3] = (a + 2000 > b) && (c < d + 300);
+        res[4] = !(!res[0]); 
+        res[5] = (a > b) ^ (c < d);
+        return res;
+
+    }
+} 
---
+++ Tyuiu.RotachAO.Sprint2.Task1.V13/Program.cs
@@ -0,0 +1,37 @@
+using Tyuiu.RotachAO.Sprint2.Task1.V13.Lib; 
+ 
+namespace Tyuiu.BarbashinAV.Sprint2.Task1.V13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int a = 145;
+        int b = 916;
+        int c = 164;
+        int d = 137;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+        Console.Title = "Спринт #2 | Выполнил: Ротач. А. О. | ИИПб-24-1";
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* Спринт #2                                                            *");
+        Console.WriteLine("* Тема:                                                                *");
+        Console.WriteLine("* Задание #1                                                           *");
+        Console.WriteLine("* Вариант #13                                                          *");
+        Console.WriteLine("* Выполнил: Ротач Анастасия Олеговна  | ИИПб-24-1                      *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                             *");
+        Console.WriteLine("*                                                                      *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("************************************************************************");
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+        Console.ReadKey();
+    }
+
+} 
