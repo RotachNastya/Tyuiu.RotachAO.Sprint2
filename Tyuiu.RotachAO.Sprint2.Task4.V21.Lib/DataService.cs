1970c929a63afd2275dfe7526b5ba751b718e001
RotachNastya <education.for.nastya@gmail.com>
2024-10-23 20:21:55 +0500

    Task4

--- Tyuiu.RotachAO.Sprint2.Task4.V21.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint2.Task4.V21.Lib/DataService.cs
@@ -6,7 +6,7 @@ public class DataService : ISprint2Task4V21
 {
     public double Calculate(double x, double y)
     {
-        double z = x < y - 2 ? (Math.Pow(2 + (x + 1) / Math.Pow(x, 2), y)) : ((Math.Pow(x, 2)) + 10) / (Math.Pow(y, 2) - (Math.Sin(x*x) + 12));
+        double z = x < y - 2 ? (Math.Pow(2 + (x + 1) / Math.Pow(x, 2), y)) : ((Math.Pow(x, 2)) + 10) / (Math.Pow(y, 2) - Math.Sin(x * x) + 12);
         return Math.Round(z, 3);
     }
 } 
