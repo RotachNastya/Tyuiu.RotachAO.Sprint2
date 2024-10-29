5858c0639793a8a297dfc262dd438ec9a4cd6b0e
RotachNastya <education.for.nastya@gmail.com>
2024-10-29 13:13:44 +0500

    Task5

--- Tyuiu.RotachAO.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint2.Task5.V2.Lib/DataService.cs
@@ -11,19 +11,19 @@ public class DataService : ISprint2Task5V2
             case 1:
             case 2:
             case 12:
-                return "Зима";
+                return "зима";
             case 3:
             case 4:
             case 5:
-                return "Весна";
+                return "весна";
             case 6:
             case 7:
             case 8:
-                return "Лето";
+                return "лето";
             case 9:
             case 10:
             case 11:
-                return "Осень";
+                return "осень";
             default:
                 throw new ArgumentException($"Месяц должен быть от 1 до 12.Значение {value}");
         }
