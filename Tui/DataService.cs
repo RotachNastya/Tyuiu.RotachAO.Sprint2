807b51253fc0ef3f38cb00087cb14e4957f94727
RotachNastya <education.for.nastya@gmail.com>
2024-10-29 13:07:42 +0500

    Task5

---
+++ Tui/DataService.cs
@@ -0,0 +1,31 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.RotachAO.Sprint2.Task5.V2.Lib;
+
+public class DataService : ISprint2Task5V2
+{
+    public string FindMonthSeason(int value)
+    {
+        switch (value)
+        {
+            case 1:
+            case 2:
+            case 12:
+                return "Зима";
+            case 3:
+            case 4:
+            case 5:
+                return "Весна";
+            case 6:
+            case 7:
+            case 8:
+                return "Лето";
+            case 9:
+            case 10:
+            case 11:
+                return "Осень";
+            default:
+                throw new ArgumentException($"Месяц должен быть от 1 до 12.Значение {value}");
+        }
+    }
+}
