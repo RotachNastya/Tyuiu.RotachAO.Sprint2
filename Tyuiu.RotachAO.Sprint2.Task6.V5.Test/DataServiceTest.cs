b48d332ae1053ab113661a63366fe3aab4e3e7f0
RotachNastya <education.for.nastya@gmail.com>
2024-10-30 14:19:57 +0500

    Task6

---
+++ Tyuiu.RotachAO.Sprint2.Task6.V5.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.RotachAO.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.RotachAO.Sprint2.Task6.V5.Test;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void ValidExpression()
+    {
+        DataService ds = new DataService();
+        Assert.AreEqual("øåñòåðêà", ds.FindCardValue(6));
+        Assert.AreEqual("ñåìåðêà", ds.FindCardValue(7));
+        Assert.AreEqual("âîñüìåðêà", ds.FindCardValue(8));
+        Assert.AreEqual("äåâÿòêà", ds.FindCardValue(9));
+        Assert.AreEqual("äåñÿòêà", ds.FindCardValue(10));
+        Assert.AreEqual("âàëåò", ds.FindCardValue(11));
+        Assert.AreEqual("äàìà", ds.FindCardValue(12));
+        Assert.AreEqual("êîðîëü", ds.FindCardValue(13));
+        Assert.AreEqual("òóç", ds.FindCardValue(14));
+        Assert.ThrowsException<ArgumentException>(() => ds.FindCardValue(5));
+        Assert.ThrowsException<ArgumentException>(() => ds.FindCardValue(15));
+    }
+}
---
+++ Tyuiu.RotachAO.Sprint2.Task6.V5/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.RotachAO.Sprint2.Task6.V5.Lib;
+
+namespace Tyuiu.RotachAO.Sprint2.Task6.V5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService dataService = new DataService();
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* Спринт #2                                                            *");
+        Console.WriteLine("* Тема:                                                                *");
+        Console.WriteLine("* Задание #6                                                           *");
+        Console.WriteLine("* Вариант #5                                                          *");
+        Console.WriteLine("* Выполнил: Ротач Анастасия Олеговна  | ИИПб-24-1                                                           *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* Тема: Получение результата из switch                                 *");
+        Console.WriteLine("* Задание #6                                                           *");
+        Console.WriteLine("* Вариант #5                                                           *");
+        Console.WriteLine("* Выполнила: Сырцова Софья Алексеевна | ИИПб-24-1                      *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ: Игральным картам условно присвоены следующие порядковые     *");
+        Console.WriteLine("* номера в зависимости от их достоинства : «валету» — 11, «даме» — 12, *");
+        Console.WriteLine("* «королю» — 13, «тузу» — 14. Порядковые номера остальных карт         *");
+        Console.WriteLine("* соответствуют их названиям («шестерка» и т.п.). По заданному номеру  *");
+        Console.WriteLine("* карты k (6 <=k <= 14) определить достоинство соответствующей карты.  *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                     *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("Введите номер карты:");
+        int n = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+        Console.WriteLine("************************************************************************");
+        Console.WriteLine($"Карта = {dataService.FindCardValue(n)}");
+    }
+}
